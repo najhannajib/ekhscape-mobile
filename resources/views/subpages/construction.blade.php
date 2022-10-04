f994d967853606d22d006ad8e9268616dd6be84d
njhn-pnsb <najhan.najib@pipeline-network.com>
2022-10-05 04:07:24 +0800

    out to on

--- resources/views/subpages/construction.blade.php
+++ resources/views/subpages/construction.blade.php
@@ -5,7 +5,7 @@
             <div class="card">
                 <div class="card-body">
                     <h1 class="h1">Sorry, this site is still in progress.</h1>
-                    <p>But don't worry, you can check out others</p>
+                    <p>But don't worry, you can check on others</p>
                     <img src="../../../img/icon-es/g1.gif" alt="map" class="w-100 border-radius-lg">
                 </div>
             </div>
